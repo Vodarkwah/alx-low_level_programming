92243a0a9fcbbabac66a36efa0887a0a6404de85
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-03 21:30:05 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l, r;
+
+	for (l = 48;  r <= 78; l++)
+	{
+	for (r = l + 1 ; r <= 78; r++)
+	{
+		putchar(r);
+		putchar(l0);
+
+		if ((l == 50) && (r == 78))
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+}
