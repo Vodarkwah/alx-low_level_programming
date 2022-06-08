918f262a736f556e0f367bae7e8af2bd6b5d6422
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-08 15:19:34 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,11 @@
 #include "main.h"
+/**
+ * main - prints _putchar and a new line
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	_putchar("_putchar");
+	_putchar("\n_\np\nu\nt\nc\nh\na\nr\n");
 	return (0);
-}/*just trying something out*/
+}
