8063ad71af9bf06248666f68780fae0127b69c9b
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-09 17:26:47 -0700

    initial commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers between 0 to 14 
- * 10 times.
+ * more_numbers - prints numbers between 0 to 14 10 times.
+ *
  * Return: no return.
  */
 void more_numbers(void)
