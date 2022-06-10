0a68afb36356b0c1870102ac0b111716c1f3cee4
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-09 18:10:31 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/100_prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+	long int n, prime_fac;
+
+	n = 612852475143;
+	for (prime_fac = 2; prime_fac <= n; prime_fac++)
+	{
+		if (n % prime_fac == 0)
+		{
+			n /= prime_fac;
+			prime_fac--;
+		}
+	}
+	printf("%ld\n", prime_fac);
+	return (0);
+}
