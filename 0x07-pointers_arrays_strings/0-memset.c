ef8d829cdf83301bef8edbe83637eff9741960ab
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-21 17:41:44 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - This function fills the first n bytes of the 
+ * memory area pointed to by s with the constant byte b
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
+}
