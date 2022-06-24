0cbb3873611b8b4f529aa582bb8236a8edfb947c
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-06-23 18:02:54 -0700

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion: prints a string, followed by a new line.
+ *
+ * Return: no return.
+ */
+
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
