d3ad1b67c1dc7c93105930f5c3b15c62c81ea0cf
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-07-02 02:41:40 -0700

    initial commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		for (i = (len - 1) / 2; i < (len); i++)
 			_putchar(str[i]);
 	}
-	else
+	else if (len % 2 == 0)
 	{
 		for (i = (len) / 2; i < (len); i++)
 			_putchar(str[i]);
