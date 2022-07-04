5aadebbb20376af46a948a9c7bdc8d2c421ef35a
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-07-04 12:32:21 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,7 +24,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
 
 #endif
